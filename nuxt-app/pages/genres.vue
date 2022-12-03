576de8cb217f9dfe5f23bad9b28d398629bacda8
Maciej Błędkowski <pub@mble.dk>
2022-12-03 17:36:27 +0100

    Create first iteration of genres page

--- nuxt-app/pages/genres.vue
+++ nuxt-app/pages/genres.vue
@@ -1,5 +1,45 @@
-<template><h2>Test</h2></template>
+<template>
+  <div v-if="data">
+    <h1>Genres</h1>
+    <ul>
+      <li v-for="genre in data.data" :key="genre.id">
+        <nuxt-link :to="`/genres/${genre.id}`">{{ genre.name }}</nuxt-link>
+        <img :src="genre.picture" alt="{{genre.name}} genre image" />
+      </li>
+    </ul>
+  </div>
+</template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+div {
+  display: flex;
+  flex-flow: column nowrap;
+  h1 {
+    margin: 0;
+    font-size: 1.5rem;
+  }
+  ul {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    list-style: none;
+    padding: 0;
+    li {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 1rem;
+      img {
+        width: 100px;
+        height: 100px;
+      }
+    }
+  }
+}
+</style>
 
-<script lang="ts"></script>
+<script lang="ts" setup>
+const { data } = await useFetch<{
+  data: { id: number; name: string; picture: string }[];
+}>(() => "https://api.deezer.com/genre");
+</script>
