76d43ab2a70c951c450650510eb80a87f258c505
mbledkowski <pub@mble.dk>
2023-01-04 02:58:17 +0100

    Update genres genre page to use LastFM API

--- nuxt-app/pages/genres/[id].vue
+++ nuxt-app/pages/genres/[id].vue
@@ -3,20 +3,17 @@
     <h1>Genre</h1>
     <h2>
       {{ genre.name }}
-      <span class="gray">
-        {{ genre.id }}
-      </span>
     </h2>
-    <img :src="genre.picture_xl" />
+    <img :src="`https://source.unsplash.com/512x512/?${genre.name.split(' ').join('-')}-music`" />
+    <p v-html="genre.wiki.content">
+    </p>
   </div>
   <div v-else>Please refresh the webpage!</div>
 </template>
 
 <script lang="ts" setup>
 const route = useRoute();
-const { data: genre } = await useFetch<{
-  id: number;
-  name: string;
-  picture_xl: string;
-}>(() => `https://api.deezer.com/genre/${route.params.id}`);
+const {data: genresData} = await useFetch<{tag: { name: string; count: number; reach: number; wiki: {summary: string; content: string;}
+}}>(() => `https://ws.audioscrobbler.com/2.0/?method=tag.getinfo&tag=${route.params.id}&api_key=e2ec98b041891e5f1163138c3515c552&format=json`);
+const genre = genresData.value?.tag;
 </script>
