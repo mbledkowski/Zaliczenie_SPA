9d8626fab3866c63dbabad8105f6a1e468d01738
mbledkowski <pub@mble.dk>
2023-01-04 05:16:03 +0100

    Add margin to main page

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="hero min-h-full bg-base-200">
-  <div class="hero-content flex-col">
+  <div class="hero-content flex-col m-12">
     <div>
       <h1 class="text-5xl font-bold">Music info 🎧</h1>
       <p class="py-6">Find information about music!<br/>To get started select the most interesting topic in the navigation.</p>
